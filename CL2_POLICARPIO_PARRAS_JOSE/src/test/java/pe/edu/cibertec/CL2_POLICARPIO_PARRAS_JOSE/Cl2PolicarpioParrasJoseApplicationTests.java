8a76233d560fa23e0f169f0e28d28872286e69de
joparras <this.jmiguel_150email@gmail.com>
2023-07-19 19:40:41 -0500

    Creacion de proyecto

---
+++ CL2_POLICARPIO_PARRAS_JOSE/src/test/java/pe/edu/cibertec/CL2_POLICARPIO_PARRAS_JOSE/Cl2PolicarpioParrasJoseApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL2_POLICARPIO_PARRAS_JOSE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2PolicarpioParrasJoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
